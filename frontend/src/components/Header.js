9540d40a3e9200d1a0e451d175bdd509931e9e0e
opeyemibami <bamigbadeopeyemi@gmail.com>
2021-07-04 21:34:07 +0100

    add homeScreen component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,12 +9,12 @@ const Header = () => {
           <Navbar.Brand href='/'>Proshop</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto'>
+            <Nav className='ms-auto'>
               <Nav.Link href='/cart'>
                 <i className='fas fa-shopping-cart'></i>Cart
               </Nav.Link>
               <Nav.Link href='/login'>
-                <i className='fas fa-user'></i>Sign In{' '}
+                <i className='fas fa-user'></i>Sign In
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
