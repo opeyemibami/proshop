593e7806b3f8c475a93831d6c97eaa78cfd8acb9
opeyemibami <bamigbadeopeyemi@gmail.com>
2021-07-04 22:31:37 +0100

    add rating stars

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({product}) => {
     return (
@@ -13,14 +14,14 @@ const Product = ({product}) => {
             </Card.Title>
           </a>
           <Card.Text as='div'>
-            <div className='my-3'>
-              {product.rating} from {product.numReviews} views
-            </div>
+            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
           </Card.Text>
           <Card.Text as='h3'>${product.price}</Card.Text>
         </Card.Body>
       </Card>
     )
 }
+
+
 
 export default Product
