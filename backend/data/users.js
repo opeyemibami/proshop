33a6cfe57b1df10ee2b56b782f6e03f616231efe
opeyemibami <bamigbadeopeyemi@gmail.com>
2021-07-12 11:14:57 +0100

    temporary data import and destroy

---
+++ backend/data/users.js
@@ -0,0 +1,34 @@
+import bcrypt from 'bcryptjs'
+const hashPass = async (password) => {
+  return await bcrypt.hash(password,8)
+}
+
+const pass1 = await hashPass('123456')
+console.log(pass1)
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    passowrd: pass1,
+    isAdmin: true,
+  },
+  {
+    name: 'John doe',
+    email: 'john@example.com',
+    passowrd: pass1,
+  },
+  {
+    name: 'James doe',
+    email: 'james@example.com',
+    passowrd: pass1,
+  },
+  {
+    name: 'Jane doe',
+    email: 'jane@example.com',
+    passowrd: pass1,
+  },
+]
+
+
+export default users
