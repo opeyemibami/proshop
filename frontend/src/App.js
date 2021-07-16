7496a0f0eea4789dc3ee7a2e36cd2aaefabd7828
opeyemibami <bamigbadeopeyemi@gmail.com>
2021-07-16 14:19:06 +0100

    cartReducer and addTocart actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,17 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
     <Router>
       <Header />
       <main className='py-3'>
-        <Container style={{position : 'relative', top: "128px"}}>
+        <Container style={{ position: 'relative', top: '128px' }}>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/Cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,14 +2,20 @@ import {createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import  {composeWithDevTools} from 'redux-devtools-extension'
 import { productListReducer, productDetailReducer} from './reducers/productReducers'
+import {cartReducer} from './reducers/cartReducers'
 
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailReducer,
+  cart: cartReducer,
 })
 
-const initialState = {}
+const cartItermFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartIterms')) : []
+
+const initialState = {
+  cartItems: cartItermFromStorage,
+}
 const middleware = [thunk]
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
 
