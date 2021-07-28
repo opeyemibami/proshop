3f70384d891f1a307e59575e057db0935840eb80
opeyemibami <bamigbadeopeyemi@gmail.com>
2021-07-28 10:44:00 +0100

    implement user proflle screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/loginScreen'
 import RegisterScreen from './screens/registerScreen'
+import ProfileScreen from './screens/profileScreen'
 
 const App = () => {
   return (
@@ -15,8 +16,9 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container style={{ marginTop: '8rem' }}>
-          <Route path='/login/' component={LoginScreen} />
-          <Route path='/register/' component={RegisterScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
